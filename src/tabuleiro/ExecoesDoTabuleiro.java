2951f05a42e407be17efde79924b4f55ba0b2351
tallisrafael <tallis_rafael25@hotmail.com>
2020-08-25 11:34:57 -0300

    implementando execoes no tabuleiro

---
+++ src/tabuleiro/ExecoesDoTabuleiro.java
@@ -0,0 +1,11 @@
+package tabuleiro;
+
+public class ExecoesDoTabuleiro extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ExecoesDoTabuleiro(String msg){
+		 super(msg);
+	 }
+	
+
+}
--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -6,6 +6,10 @@ public class Tabuleiro {
 	private Peca[][] pecas;
 
 	public Tabuleiro(int linhas, int colunas) {
+		if (linhas < 1 || colunas < 1) {
+			throw new ExecoesDoTabuleiro(
+					"Erro ao criar o tabuleiro: o tabuleiro tem que ter no minimo uma linha e uma coluna");
+		}
 		this.linhas = linhas;
 		this.colunas = colunas;
 		pecas = new Peca[linhas][colunas];
@@ -15,29 +19,42 @@ public class Tabuleiro {
 		return linhas;
 	}
 
-	public void setLinhas(int linhas) {
-		this.linhas = linhas;
-	}
-
 	public int getColunas() {
 		return colunas;
 	}
 
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
-
-	public Peca peca(int linhas, int colunas) {
-		return pecas[linhas][colunas];
+	public Peca peca(int linha, int coluna) {
+		if (!posicaoExiste(linha, coluna))
+			throw new ExecoesDoTabuleiro("Nenhuma posicao foi encontrada");
+		return pecas[linha][coluna];
 	}
 
 	public Peca peca(Posicao posicao) {
+		if (!posicaoExiste(posicao))
+			throw new ExecoesDoTabuleiro("Nenhuma posicao foi encontrada");
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
 
 	public void colocarPeca(Peca peca, Posicao posicao) {
+		if(temPecaNaPosicao(posicao)) {
+			throw new ExecoesDoTabuleiro("Já existe uma peca nessa posicao " + posicao);
+		}
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
+	}
+
+	private boolean posicaoExiste(int linha, int coluna) {
+		return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+	}
+
+	public boolean posicaoExiste(Posicao posicao) {
+		return posicaoExiste(posicao.getLinha(), posicao.getColuna());
+	}
+
+	public boolean temPecaNaPosicao(Posicao posicao) {
+		if (!posicaoExiste(posicao))
+			throw new ExecoesDoTabuleiro("Nenhuma posicao foi encontrada");
+		return peca(posicao) != null;
 	}
 
 }
